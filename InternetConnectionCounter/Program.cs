034163ed99067497f20be22bd29204c174a10ade
Jay <jbtamaresgit@outlook.com>
2019-05-26 23:20:59 +0800

    Add project files.

---
+++ InternetConnectionCounter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace InternetConnectionCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = String.Empty;
+            int counter = 0;
+
+            while (true)
+            {
+                if (!NetworkInterface.GetIsNetworkAvailable())
+                {
+                    counter++;
+                    Console.WriteLine($"{counter} - {DateTime.Now.ToString()}");
+                    Thread.Sleep(120000);
+                }
+            }
+        }
+    }
+}
